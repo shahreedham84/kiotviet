3e9b624bd5e52543801168e9ab01896b06b4438d
shahreedham84 <shahreedham84@gmail.com>
2025-10-22 22:45:22 +0700

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,25 +6,18 @@ const urlsToCache = [
   './products.js',
   './tables_orders.js',
   './hamburger.js',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-// Install
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
-// Fetch
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
 
-// Activate
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
@@ -32,4 +25,3 @@ self.addEventListener('activate', event => {
     )
   );
 });
-
